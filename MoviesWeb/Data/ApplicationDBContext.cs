848541b4161ba5913f0aab172636d7f979574cad
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2023-02-16 19:42:09 +0200

    Implementing Category Model & DBContext (Code first) Migration to DB

---
+++ MoviesWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesWeb.Models;
+
+namespace MoviesWeb.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
