d9825a572968c889288862d2dd7e29af1607e395
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2023-02-17 10:12:34 +0200

    Implementing Get All Categories

---
+++ MoviesWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesWeb.Data;
+using MoviesWeb.Models;
+
+namespace MoviesWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public CategoryController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _dbContext.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
