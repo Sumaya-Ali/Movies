6d2ddf3f225eac6453667bb131f39f961c42f38c
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2023-02-21 14:05:02 +0200

    Implementing Add new Category with Server side Validation

--- MoviesWeb/Controllers/CategoryController.cs
+++ MoviesWeb/Controllers/CategoryController.cs
@@ -18,5 +18,28 @@ namespace MoviesWeb.Controllers
             IEnumerable<Category> objCategoryList = _dbContext.Categories;
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("name", "Display order can't exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(obj);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
     }
 }
